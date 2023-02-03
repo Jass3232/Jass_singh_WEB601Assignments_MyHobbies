c05448488f11457515b6f54f12b155aa393496a5
Jass3232 <100043886+Jass3232@users.noreply.github.com>
2023-02-02 20:34:37 -0500

    scss pending

--- My-Hobbies/src/app/app.module.ts
+++ My-Hobbies/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HobbiesComponent } from './MyComponents/hobbies/hobbies.component';
+
 import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HobbiesComponent,
-    ContentCardComponent
+
+    ContentCardComponent,
+     ContentListComponent
   ],
   imports: [
     BrowserModule
